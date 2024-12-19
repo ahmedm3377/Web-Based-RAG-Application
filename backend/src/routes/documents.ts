b163343af379704e041663d00739e71ee459c3cd
Ahmed Ibrahim <ahmedm3377@yahoo.com>
2024-12-19 10:31:08 -0600

    Updated storage path in multer

--- backend/src/routes/documents.ts
+++ backend/src/routes/documents.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import multer from "multer";
 import { query_handler, summarize_handler, upload_handler } from '../controllers/docController';
+import fs from 'fs';
 
 
 // Storage option is used here to keep the original filename
 const storage = multer.diskStorage({ 
     destination: function (req, file, cb) {
+        const pth = 'uploads';
+        fs.mkdirSync(pth, { recursive: true });
         cb(null, 'uploads');
+        console.log(cb(null, pth));
     },
     filename: function(req, file, cb) {
         cb(null, file.originalname);
